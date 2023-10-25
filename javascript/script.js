802b04f654e6de83ae3c17518e5ec1312ee98d49
Danish Pramuditya <darizq37@gmail.com>
2023-10-26 05:44:35 +0700

    [DA] feat: add cullinary

---
+++ javascript/script.js
@@ -0,0 +1,14 @@
+function food() {
+    document.getElementById("food").style.backgroundColor = "#101010";
+    document.getElementById("drink").style.backgroundColor = "#F8F6F0";
+    document.getElementById("food-item").style.display = "block";
+}
+
+function drink() {
+    document.getElementById("drink").style.backgroundColor = "#101010";
+    document.getElementById("food").style.backgroundColor = "#F8F6F0";
+    document.getElementById("food-item").style.display = "none";
+}
+
+
+
