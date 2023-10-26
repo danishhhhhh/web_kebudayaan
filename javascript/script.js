c860bc021d28b4a36be4d9256d350e92f79d907f
Danish Pramuditya <darizq37@gmail.com>
2023-10-26 07:32:37 +0700

    [DA] feat: add someting component

--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,22 @@
-function food() {
+
+
+
+
+document.getElementById("food").addEventListener('click', () => {
     document.getElementById("food").style.backgroundColor = "#101010";
     document.getElementById("drink").style.backgroundColor = "#F8F6F0";
     document.getElementById("food-item").style.display = "block";
-}
+    document.getElementById("drink-item").style.display = "none";
+})
 
-function drink() {
+document.getElementById("drink").addEventListener('click', () => {
     document.getElementById("drink").style.backgroundColor = "#101010";
     document.getElementById("food").style.backgroundColor = "#F8F6F0";
     document.getElementById("food-item").style.display = "none";
-}
+    document.getElementById("drink-item").style.display = "block";
+})
+
+
 
 
 
