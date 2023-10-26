70bcd5589731628682666737b38723da2951da4d
Danish Pramuditya <darizq37@gmail.com>
2023-10-26 20:48:43 +0700

    [DA] fix: style cullinary tab

--- javascript/script.js
+++ javascript/script.js
@@ -1,26 +1,19 @@
 
-
-
-
 document.getElementById("food").addEventListener('click', () => {
-    document.getElementById("food").style.backgroundColor = "#101010";
-    document.getElementById("drink").style.backgroundColor = "#F8F6F0";
-    document.getElementById("food").style.color = "#F8F6F0";
-    document.getElementById("drink").style.color = "#101010";
-    document.getElementById("food-item").style.display = "block";
-    document.getElementById("drink-item").style.display = "none";
-})
+    handleTabClick("food", "food-item");
+});
 
 document.getElementById("drink").addEventListener('click', () => {
-    document.getElementById("drink").style.backgroundColor = "#101010";
-    document.getElementById("food").style.backgroundColor = "#F8F6F0";
-    document.getElementById("drink").style.color = "#F8F6F0";
-    document.getElementById("food").style.color = "#101010";
-    document.getElementById("food-item").style.display = "none";
-    document.getElementById("drink-item").style.display = "block";
-})
+    handleTabClick("drink", "drink-item");
+});
 
+function handleTabClick(tabId, contentId) {
+    document.getElementById(tabId).style.backgroundColor = "#101010";
+    document.getElementById(tabId).style.color = "#F8F6F0";
+    document.getElementById(contentId).style.display = "block";
 
-
-
-
+    const otherTabId = tabId === "food" ? "drink" : "food";
+    document.getElementById(otherTabId).style.backgroundColor = "#F8F6F0";
+    document.getElementById(otherTabId).style.color = "#101010";
+    document.getElementById(otherTabId + "-item").style.display = "none";
+}
