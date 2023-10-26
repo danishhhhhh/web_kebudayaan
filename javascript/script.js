5779bda9061a08028bdc9b633c693b8828c23ef0
Danish Pramuditya <darizq37@gmail.com>
2023-10-26 08:32:58 +0700

    [DA] style: give cullinary tab button style

--- javascript/script.js
+++ javascript/script.js
@@ -5,6 +5,8 @@
 document.getElementById("food").addEventListener('click', () => {
     document.getElementById("food").style.backgroundColor = "#101010";
     document.getElementById("drink").style.backgroundColor = "#F8F6F0";
+    document.getElementById("food").style.color = "#F8F6F0";
+    document.getElementById("drink").style.color = "#101010";
     document.getElementById("food-item").style.display = "block";
     document.getElementById("drink-item").style.display = "none";
 })
@@ -12,6 +14,8 @@ document.getElementById("food").addEventListener('click', () => {
 document.getElementById("drink").addEventListener('click', () => {
     document.getElementById("drink").style.backgroundColor = "#101010";
     document.getElementById("food").style.backgroundColor = "#F8F6F0";
+    document.getElementById("drink").style.color = "#F8F6F0";
+    document.getElementById("food").style.color = "#101010";
     document.getElementById("food-item").style.display = "none";
     document.getElementById("drink-item").style.display = "block";
 })
